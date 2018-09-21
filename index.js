82155511f5ed070dd66bc52ce9c40414dea6da17
Petri Kallio <kallionpetri@gmail.com>
2018-09-21 08:07:18 +0300

    Tehtävät 3.16

--- index.js
+++ index.js
@@ -101,10 +101,14 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+    Person
+        .findByIdAndRemove(req.params.id)
+        .then(result => {
+            res.status(204).end()
+        })
+        .catch(error => {
+            res.status(400).send({ error: 'Malformatted id' })
+        })
 })
 
 const PORT = process.env.PORT || 3001
